b987c33dd3319c913aab90e3306165d29a88456e
ysfkel <yusufkel@gmail.com>
2024-03-15 20:37:08 +0400

    added amm

---
+++ protocol/module/amm/src/adapters/amm.rs
@@ -0,0 +1,59 @@
+use soroban_sdk::{Address,Symbol, Vec, Val,contracttype, contractclient, contractspecfn };
+
+
+pub use IAMMClient as Client;
+pub use IAMM as Interface;
+
+#[contracttype]
+pub struct CallData {
+    target: Address,
+    method: Symbol,
+    data: Vec<Val>,
+}
+
+#[contractclient(name="IAMMClient")]
+#[contractspecfn(name = "IMMSpec")]
+pub trait IAMM {
+    fn get_call_data() ->CallData ;
+}
+
+/// Spec contains the contract spec of iamm contracts, including the general
+/// interface, as well as the admin interface, such as the Stellar Asset
+/// Contract.
+#[doc(hidden)]
+pub struct  IMMSpec;
+
+pub(crate) const SPEC_XDR_INPUT: &[&[u8]] = &[ 
+    &IMMSpec::spec_xdr_get_call_data(),
+];
+
+pub(crate) const SPEC_XDR_LEN: usize = 5336;
+
+impl IMMSpec {
+    /// Returns the XDR spec for the Token contract.
+    pub const fn spec_xdr() -> [u8; SPEC_XDR_LEN] {
+        let input = SPEC_XDR_INPUT;
+        // Concatenate all XDR for each item that makes up the imm spec.
+        let mut output = [0u8; SPEC_XDR_LEN];
+        let mut input_i = 0;
+        let mut output_i = 0;
+        while input_i < input.len() {
+            let subinput = input[input_i];
+            let mut subinput_i = 0;
+            while subinput_i < subinput.len() {
+                output[output_i] = subinput[subinput_i];
+                output_i += 1;
+                subinput_i += 1;
+            }
+            input_i += 1;
+        }
+
+        // Check that the numbers of bytes written is equal to the number of bytes
+        // expected in the output.
+        if output_i != output.len() {
+            panic!("unexpected output length",);
+        }
+
+        output
+    }
+}
